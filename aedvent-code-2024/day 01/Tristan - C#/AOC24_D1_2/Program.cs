30ca7fd97cbac09c61c2f63d74e712adc34f7f7a
Tristan <tristan.cuvelier@ae.be>
2024-12-09 08:58:57 +0100

    Tristan - Day 1

---
+++ aedvent-code-2024/day 01/Tristan - C#/AOC24_D1_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+string input;
+
+try
+{
+    StreamReader sr = new StreamReader("../../../../../../../input/day1_1.txt");
+    input = sr.ReadLine();
+
+    List<int> list1 = new List<int>();
+    List<int> list2 = new List<int>();
+
+    while (input != null)
+    {
+        int spaceIndex = input.IndexOf(' ');
+
+        int number1 = int.Parse(input.Substring(0, spaceIndex));
+        int number2 = int.Parse(input.Substring(spaceIndex + 1));
+
+        list1.Add(number1);
+        list2.Add(number2);
+
+        input = sr.ReadLine();
+    }
+    sr.Close();
+
+    System.Console.WriteLine("Got all input into lists...");
+
+    list1.Sort();
+    list2.Sort();
+
+    var mappedResult = list1.Select((x) => x * list2.Count(y => y == x));
+
+    int res = mappedResult.Select(Math.Abs).Sum();
+    System.Console.WriteLine(res);
+
+}
+catch (Exception e)
+{
+    Console.WriteLine("Something went wrong: " + e);
+}
