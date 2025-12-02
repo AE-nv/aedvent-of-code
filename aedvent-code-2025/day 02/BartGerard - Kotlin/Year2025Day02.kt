ed299426158597364a4f4af35de42001a26c6a1e
bart.gerard <bart.gerard@gmail.com>
2025-12-02 06:27:56 +0100

    2025/01 Bart Gerard - Kotlin

---
+++ aedvent-code-2025/day 02/BartGerard - Kotlin/Year2025Day02.kt
@@ -0,0 +1,26 @@
+package aock2025
+
+data class Year2025Day02(
+    private val input: List<LongRange>
+) {
+    companion object {
+        val RANGE_PATTERN = "(\\d+)-(\\d+)".toRegex()
+        val PALINDROME = "(\\d+)\\1".toRegex()
+        val REPEATED_AT_LEAST_TWICE = "(\\d+)\\1+".toRegex()
+
+        private fun String.toLongRanges() = RANGE_PATTERN.findAll(this)
+            .map { it.destructured }
+            .map { (start, end) -> start.toLong()..end.toLong() }
+            .toList()
+    }
+
+    constructor(input: String) : this(input.replace("\r", "").toLongRanges())
+
+    fun partOne() = filterByPattern(PALINDROME).sum()
+
+    fun partTwo() = filterByPattern(REPEATED_AT_LEAST_TWICE).sum()
+
+    private fun filterByPattern(pattern: Regex) = input.flatMap { id ->
+        id.filter { it.toString().matches(pattern) }
+    }
+}
